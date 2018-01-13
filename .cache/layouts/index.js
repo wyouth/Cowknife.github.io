693b9d544f64a76e752f2aa302bbe2208b3e54a6
冯昊 <fh345392977@gamil.com>
2018-01-13 23:38:50 +0800

    修改 ignal

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/analyzer/Desktop/tutorial-part-one/lumen-blog/src/layouts/index.jsx"
+  import data from "/Users/analyzer/Desktop/tutorial-part-one/lumen-blog/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
